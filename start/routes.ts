7191696ed9e7a8dc3ea32d9cb43be966f6b3988a
Sean Shoffstall <sean@sycle.net>
2021-02-10 19:55:43 -0800

    Added Registration Controller, routes and view pages. Also installed bcrypt and argon2

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,10 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.on('/').render('homepage',  { title: 'Create a snappy page title' })
 Route.on('/login').render('auth/login',  { title: 'Login to our new application' }).as('login')
 Route.on('/register').render('auth/register',  { title: 'Register for our new application' }).as('register')
+Route.post('/register', 'AuthController.register')
+
+
+Route.get('/dashboard', async ({ auth }) => {
+  const user = await auth.authenticate()
+  return `Hello user! Your email address is ${user.email}`
+})
